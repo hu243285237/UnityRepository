54066df1e6d5aa860633eabf92b791f9349093aa
24 <243285237@qq.com>
2018-06-25 10:12:28 +0800

    Add GameRunTime and TimeCountDown

---
+++ GameTime/GameTime.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 游戏运行时间
+/// </summary>
+public class GameTime : MonoBehaviour
+{
+    private int hour;
+    private int minute;
+    private int second;
+
+    void Update() 
+    {
+        hour = (int)Time.time / 3600;
+        minute = (int)(Time.time - hour * 3600) / 60;
+        second = (int)Time.time % 60;
+
+        Debug.Log(hour + ":" + minute + ":" + second);
+    }
+}
---
+++ GameTime/TimeCountDown.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 游戏倒计时
+/// </summary>
+public class TimeCountDown : MonoBehaviour
+{
+    //指定秒数
+    public float specifySecond = 100;
+
+    private int hour;
+    private int minute;
+    private int second;
+
+    //----------------------------------------------------------
+
+    void Update() 
+    {
+        if(specifySecond >= 0)
+        {
+            hour = (int)specifySecond / 3600;
+            minute = (int)(specifySecond - hour * 3600) / 60;
+            second = (int)specifySecond % 60;
+
+            specifySecond -= Time.deltaTime;
+
+            Debug.Log(hour + ":" + minute + ":" + second);
+        }
+    }
+
+    //----------------------------------------------------------
+}
