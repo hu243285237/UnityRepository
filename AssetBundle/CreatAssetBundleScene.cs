55317625359bb13e927abc5cb240b12ad9bda120
24 <243285237@qq.com>
2018-07-04 10:18:04 +0800

    Add CreatAssetBundleScene and Load
    but here have a problem , although scene can be creat and load , but the script can't , must copy script. BuildOption have a parm is BuildScriptOnly , but I dont know how to use it , it will creat a assetbundle and a _Data file.

---
+++ AssetBundle/CreatAssetBundleScene.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEditor;
+
+/// <summary>
+/// 创建AssetBundle,此脚本放在Editor文件夹
+/// </summary>
+public class CreatAssetBundleScene : Editor
+{
+    [MenuItem("Assets/Build AssetBundle Scene")]
+    static void MyBuild()
+    {
+        string[] levels = { "Assets/MyScene/Electromagnetism.unity" };
+
+        BuildPipeline.BuildPlayer(levels, "AssetBundle/Electromagnetism.ab", BuildTarget.StandaloneWindows64, BuildOptions.BuildAdditionalStreamedScenes);
+
+        AssetDatabase.Refresh();//刷新编辑器界面
+    }
+}
---
+++ AssetBundle/LoadAssetBundle.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// 加载AssetBundle
+/// </summary>
+public class LoadAssetBundle : MonoBehaviour
+{
+    private string url;
+
+    private string assetName;
+
+    void Start()
+    {
+        url = "file://" + Application.dataPath + "/AssetBundle/Electromagnetism.ab";
+
+        assetName = "Electromagnetism";
+
+        StartCoroutine(DownLoad());
+    }
+
+    IEnumerator DownLoad()
+    {
+        WWW www = new WWW(url);
+        yield return www;
+
+        if (!www.isDone)
+        {
+            Debug.Log("下载失败，错误信息：" + www.error);
+        }
+        else
+        {
+            Debug.Log("下载成功");
+
+            AssetBundle bundle = www.assetBundle;
+
+            SceneManager.LoadScene(assetName);
+
+            //如果不"//"这句代码,场景有可能会无法加载
+            //bundle.Unload(false);
+        }
+
+        //处理掉正在加载过程中的www
+        www.Dispose();
+    }
+}
