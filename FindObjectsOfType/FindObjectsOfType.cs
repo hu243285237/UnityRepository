96541018aabc3c680ecaf67a3ee15177a2bd7742
24 <243285237@qq.com>
2018-07-05 14:34:58 +0800

    add  findobjectsoftype

---
+++ FindObjectsOfType/FindObjectsOfType.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FindObjectsOfType : MonoBehaviour
+{
+    void Start()
+    {
+        //FindObjectsOfType可以找到此场景所有符合这个Type的物体
+        AudioSource[] allAudio = FindObjectsOfType<AudioSource>();
+
+        foreach (AudioSource audio in allAudio)
+        {
+            Debug.Log(audio.name);
+        }
+    }
+}
