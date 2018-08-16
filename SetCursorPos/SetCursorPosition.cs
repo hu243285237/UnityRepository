6b1dcd12d87d3c5d5c9994f824e0415d98756c0e
24 <243285237@qq.com>
2018-08-16 15:04:39 +0800

    Add setCursorPos
    设置鼠标位置

---
+++ SetCursorPos/SetCursorPosition.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 强制设置鼠标位置
+/// </summary>
+public class SetCursorPosition : MonoBehaviour
+{
+
+    [System.Runtime.InteropServices.DllImport("user32.dll")]
+
+    public static extern int SetCursorPos(int x, int y);
+
+    void Start()
+    {
+        SetCursorPos(1920, 1080);
+    }
+}
