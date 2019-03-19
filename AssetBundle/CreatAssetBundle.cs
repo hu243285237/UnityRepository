86407e1e68c79d4636e85faa9ac85fb6c06c2153
24 <243285237@qq.com>
2019-03-19 10:35:08 +0800

    Update and rename CreatAssetBundleScene.cs to CreatAssetBundle.cs

--- AssetBundle/CreatAssetBundle.cs
+++ AssetBundle/CreatAssetBundle.cs
@@ -5,14 +5,18 @@ using UnityEditor;
 /// <summary>
 /// 创建AssetBundle,此脚本放在Editor文件夹
 /// </summary>
-public class CreatAssetBundleScene : Editor
+public class CreatAssetBundle : Editor
 {
-    [MenuItem("Assets/Build AssetBundle Scene")]
+    [MenuItem("Assets/Build AssetBundle")]
     static void MyBuild()
     {
+        //创建Scene
         string[] levels = { "Assets/MyScene/Electromagnetism.unity" };
-
         BuildPipeline.BuildPlayer(levels, "AssetBundle/Electromagnetism.ab", BuildTarget.StandaloneWindows64, BuildOptions.BuildAdditionalStreamedScenes);
+        
+        //打包资源，要在Unity编辑器设置AssetBundle名字
+        string path = @"C:\Users\Hu\Desktop";
+        BuildPipeline.BuildAssetBundles(path, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
 
         AssetDatabase.Refresh();//刷新编辑器界面
     }
